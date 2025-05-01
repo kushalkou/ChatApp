f2c3b2110416084ee47460b210bee1ad12d18d95
kushalkou <kushal2005k@gmail.com>
2025-05-02 00:38:31 +0530

    Added group messaging. Still more features need to be added.

--- src/Server.java
+++ src/Server.java
@@ -2,46 +2,41 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-
-
-public class Server{
+public class Server {
 
     static List<PrintWriter> clientWriters = new ArrayList<>();
-
     static Map<String, PrintWriter> userWriters = new HashMap<>();
+    static Map<String, Set<String>> groupMap = new HashMap<>();
 
-    public static void main(String[] args){
-        try{
+    public static void main(String[] args) {
+        try {
             ServerSocket serverSocket = new ServerSocket(1234);
             System.out.println("Server started. Waiting for clients...");
 
-            while(true){
-
+            while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client connected!");
 
                 ClientHandler clientThread = new ClientHandler(clientSocket);
                 clientThread.start();
-
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    static class ClientHandler extends Thread{
-
+    static class ClientHandler extends Thread {
         private Socket socket;
         private BufferedReader in;
         private PrintWriter out;
         private String username;
 
-        public ClientHandler(Socket socket){
+        public ClientHandler(Socket socket) {
             this.socket = socket;
         }
 
-        public void run(){
-            try{
+        public void run() {
+            try {
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 out = new PrintWriter(socket.getOutputStream(), true);
 
@@ -58,69 +53,105 @@ public class Server{
 
                 String msg;
 
-                while((msg = in.readLine()) != null) {
-                    System.out.println("Recieved from " + username + ": " + msg);
+                while ((msg = in.readLine()) != null) {
+                    System.out.println("Received from " + username + ": " + msg);
 
-                    if(msg.startsWith("@")) {
+                    if (msg.startsWith("/create_group")) {
+                        String[] tokens = msg.split("\\s+");
 
-                        int spaceIndex = msg.indexOf(' ');
-                        if(spaceIndex != -1){
-                            
-                            String targetUser = msg.substring(1, spaceIndex);
-                            String privateMsg = msg.substring(spaceIndex + 1);
-
-                            PrintWriter targetOut;
-                            synchronized (userWriters){
-                                targetOut = userWriters.get(targetUser);
-
-                            }
-
-                            if(targetOut != null) {
-                                targetOut.println("[Private from " + username + "]: " + privateMsg);
-                                out.println("[Private to " + targetUser + "]: " + privateMsg);
-                            }
-                            else {
-                                out.println("User '" + targetUser + "' not found.");
-                            }
-                        }
-                        else {
-                            out.println("Invalid private message format. Use: @username message");
+                        if (tokens.length < 3) {
+                            out.println("Usage: /create_group <group_name> <user1> <user2> ...");
+                            continue;
                         }
 
+                        String groupName = tokens[1];
+                        Set<String> groupMembers = new HashSet<>();
+
+                        groupMembers.add(username);
+                        for (int i = 2; i < tokens.length; i++) {
+                            groupMembers.add(tokens[i]);
+                        }
+
+                        synchronized (groupMap) {
+                            groupMap.put(groupName, groupMembers);
+                        }
+
+                        out.println("Group '" + groupName + "' created and members added.");
+                        continue;
                     }
-                    else {
-                        synchronized (clientWriters){
 
-                            for (PrintWriter writer : clientWriters){
-                                if(writer != out)
-                                    writer.println("[" + username + "]: " + msg);
+                    if (msg.startsWith("@")) {
+                        int spaceIndex = msg.indexOf(' ');
+                        if (spaceIndex != -1) {
+                            String target = msg.substring(1, spaceIndex);
+                            String message = msg.substring(spaceIndex + 1);
+
+                            synchronized (userWriters) {
+                                if (userWriters.containsKey(target)) {
+                                    PrintWriter targetOut = userWriters.get(target);
+                                    targetOut.println("[Private from " + username + "]: " + message);
+                                    out.println("[Private to " + target + "]: " + message);
+                                    continue;
+                                }
+                            }
+
+                            synchronized (groupMap) {
+                                if (groupMap.containsKey(target)) {
+                                    Set<String> members = groupMap.get(target);
+
+                                    if (!members.contains(username)) {
+                                        out.println("You are not a member of group '" + target + "'.");
+                                        continue;
+                                    }
+                                    
+                                    for (String member : members) {
+                                        if (!member.equals(username)) {
+                                            PrintWriter writer = userWriters.get(member);
+                                            if (writer != null) {
+                                                writer.println("[" + target + "] " + username + ": " + message);
+                                            }
+                                        }
+                                    }
+                                    continue;
+                                }
+                            }
+
+                            out.println("No user or group named '" + target + "' found.");
+                            continue;
+                        } else {
+                            out.println("Invalid format. Use: @name message");
+                            continue;
+                        }
+                    }
+
+                    synchronized (clientWriters) {
+                        for (PrintWriter writer : clientWriters) {
+                            if (writer != out) {
+                                writer.println("[" + username + "]: " + msg);
                             }
                         }
                     }
                 }
-            
-            }
-            catch(IOException e){
+
+            } catch (IOException e) {
                 e.printStackTrace();
             } finally {
-
-                try{
+                try {
                     socket.close();
-                } catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
-                synchronized (clientWriters){
+                synchronized (clientWriters) {
                     clientWriters.remove(out);
-                    if (out != null) {
-                        clientWriters.remove(out);
-                    }
                 }
-                System.out.println(username + " has disconnected.");
 
+                synchronized (userWriters) {
+                    userWriters.remove(username);
+                }
+
+                System.out.println(username + " has disconnected.");
             }
         }
     }
 }
-
-
